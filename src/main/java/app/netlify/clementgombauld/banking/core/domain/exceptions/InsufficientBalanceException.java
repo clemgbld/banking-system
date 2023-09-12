3faa43504eadf8667933773210929e186a4ebb04
cgombauld <clement.gombauld@cpexterne.org>
2023-09-12 19:43:41 +0300

    added message for insufficient balance exception

--- src/main/java/app/netlify/clementgombauld/banking/core/domain/exceptions/InsufficientBalanceException.java
+++ src/main/java/app/netlify/clementgombauld/banking/core/domain/exceptions/InsufficientBalanceException.java
@@ -1,4 +1,7 @@
 package app.netlify.clementgombauld.banking.core.domain.exceptions;
 
 public class InsufficientBalanceException extends RuntimeException{
+    public InsufficientBalanceException() {
+        super("You cannot perform this operation your balance is insufficient");
+    }
 }
