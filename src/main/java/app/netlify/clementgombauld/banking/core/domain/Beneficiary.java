5ed93898d1d825eabaa4881216ac5d79774bf0b6
cgombauld <clement.gombauld@cpexterne.org>
2023-09-19 23:50:32 +0300

    refacto with iban value object

--- src/main/java/app/netlify/clementgombauld/banking/core/domain/Beneficiary.java
+++ src/main/java/app/netlify/clementgombauld/banking/core/domain/Beneficiary.java
@@ -1,10 +1,8 @@
 package app.netlify.clementgombauld.banking.core.domain;
 
-import app.netlify.clementgombauld.banking.core.domain.exceptions.InvalidIbanException;
-import org.iban4j.Iban;
+
 
 public class Beneficiary {
-    private final String id;
 
     private final Iban iban;
 
@@ -13,19 +11,13 @@ public class Beneficiary {
     private final String name;
 
     public Beneficiary(String id, String iban, String bic,String name) {
-        this.id = id;
-        try {
-            this.iban = Iban.valueOf(iban);
-        }catch (Exception e){
-            throw new InvalidIbanException();
-        }
-
+        this.iban = new Iban(iban);
         this.bic = bic;
         this.name = name;
     }
 
     public String getIban() {
-        return iban.toString();
+        return iban.value();
     }
 
     public String getName(){
@@ -34,7 +26,7 @@ public class Beneficiary {
 
 
     boolean hasIban(String iban){
-       return getIban().equals(iban);
+       return this.iban.isSameIban(iban);
     }
 
     public boolean isInDifferentBank(String bic) {
---
+++ src/main/java/app/netlify/clementgombauld/banking/core/domain/Iban.java
@@ -0,0 +1,37 @@
+package app.netlify.clementgombauld.banking.core.domain;
+
+import app.netlify.clementgombauld.banking.core.domain.exceptions.InvalidIbanException;
+
+import java.util.Objects;
+
+public record Iban(String value) {
+
+    public Iban(String value) {
+      this.value = validateIban(value);
+    }
+
+    private String validateIban(String value){
+        try {
+            var iban = org.iban4j.Iban.valueOf(value);
+            return iban.toString();
+        }catch (Exception e){
+            throw new InvalidIbanException();
+        }
+    }
+
+    boolean isSameIban(String iban){
+        return value.equals(iban);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Iban iban)) return false;
+        return value.equals(iban.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
