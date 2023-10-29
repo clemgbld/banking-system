909228add2e5a146dd643becb2af162111dffd84
cgombauld <clement.gombauld@cpexterne.org>
2023-10-29 16:41:29 +0100

    save

--- src/main/java/app/netlify/clementgombauld/banking/account/infra/FakeExternalBankTransactionsGateway.java
+++ src/main/java/app/netlify/clementgombauld/banking/account/infra/FakeExternalBankTransactionsGateway.java
@@ -4,10 +4,10 @@ import app.netlify.clementgombauld.banking.account.domain.ExternalBankTransactio
 import app.netlify.clementgombauld.banking.account.domain.Transaction;
 import org.springframework.stereotype.Component;
 
+// would be a secondary adapter to be able to do the link with another bounded context
 @Component
 public class FakeExternalBankTransactionsGateway implements ExternalBankTransactionsGateway {
     @Override
     public void transfer(Transaction transaction, String iban, String bic) {
-
     }
 }
