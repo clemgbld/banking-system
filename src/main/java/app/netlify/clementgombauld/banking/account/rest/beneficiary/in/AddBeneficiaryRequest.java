2c2e132d0a0d342e70226952f0890602f8929981
cgombauld <clement.gombauld@cpexterne.org>
2023-10-30 09:20:50 +0100

    handle beneficiary errors

--- src/main/java/app/netlify/clementgombauld/banking/account/rest/beneficiary/in/AddBeneficiaryRequest.java
+++ src/main/java/app/netlify/clementgombauld/banking/account/rest/beneficiary/in/AddBeneficiaryRequest.java
@@ -1,4 +1,8 @@
 package app.netlify.clementgombauld.banking.account.rest.beneficiary.in;
 
-public record AddBeneficiaryRequest(String beneficiaryIban, String beneficiaryBic, String beneficiaryName) {
+import jakarta.validation.constraints.NotBlank;
+
+public record AddBeneficiaryRequest(@NotBlank(message = "Beneficiary iban is required") String beneficiaryIban,
+                                    @NotBlank(message = "Beneficiary iban is required") String beneficiaryBic,
+                                    @NotBlank(message = "Beneficiary name is required") String beneficiaryName) {
 }
--- src/test/java/app/netlify/clementgombauld/banking/account/integration/BeneficiaryControllerIT.java
+++ src/test/java/app/netlify/clementgombauld/banking/account/integration/BeneficiaryControllerIT.java
@@ -4,11 +4,13 @@ import app.netlify.clementgombauld.banking.account.integration.configuration.Acc
 import app.netlify.clementgombauld.banking.account.rest.beneficiary.BeneficiaryController;
 import app.netlify.clementgombauld.banking.account.rest.beneficiary.in.AddBeneficiaryRequest;
 import app.netlify.clementgombauld.banking.account.rest.beneficiary.out.AddBeneficiaryResponse;
+import app.netlify.clementgombauld.banking.common.rest.error.ErrorResponse;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -33,21 +35,44 @@ public class BeneficiaryControllerIT {
         String beneficiaryIban = "DE89370400440532013000";
         String beneficiaryBic = "DEUTDEFF";
         String beneficiaryName = "John Smith";
-        AddBeneficiaryRequest addBeneficiaryRequest = new AddBeneficiaryRequest(
-                beneficiaryIban,
-                beneficiaryBic,
-                beneficiaryName
-        );
 
         ResultActions result = mockMvc.perform(post("/api/v1/beneficiary")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(jsonMapper.writeValueAsString(addBeneficiaryRequest)));
+                .content(jsonMapper.writeValueAsString(new AddBeneficiaryRequest(
+                        beneficiaryIban,
+                        beneficiaryBic,
+                        beneficiaryName
+                ))));
 
         MvcResult mvcResult = result.andExpect(status().isCreated()).andReturn();
 
         String content = mvcResult.getResponse().getContentAsString();
 
         assertThat(content).isEqualTo(jsonMapper.writeValueAsString(new AddBeneficiaryResponse(beneficiaryId)));
+
+    }
+
+    @Test
+    void shouldBeABadRequestWhenTryingToAddAnAlreadyExistingBeneficiary() throws Exception {
+        String beneficiaryIban = "FR1420041010050500013M02606";
+        String beneficiaryBic = "BNPAFRPP123";
+        String beneficiaryName = "Arsene Lupin";
+
+        ResultActions result = mockMvc.perform(post("/api/v1/beneficiary")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonMapper.writeValueAsString(new AddBeneficiaryRequest(beneficiaryIban,
+                        beneficiaryBic,
+                        beneficiaryName))));
+
+        MvcResult mvcResult = result.andExpect(status().isBadRequest()).andReturn();
+
+        String content = mvcResult.getResponse().getContentAsString();
+
+        assertThat(content).isEqualTo(jsonMapper.writeValueAsString(new ErrorResponse(
+                "The beneficiary with the accountIdentifier : FR1420041010050500013M02606 is already a beneficiary of the account 4",
+                HttpStatus.BAD_REQUEST.value()
+        )));
+        
 
     }
 
