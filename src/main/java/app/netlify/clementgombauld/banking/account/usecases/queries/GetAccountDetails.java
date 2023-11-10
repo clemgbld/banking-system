4ed755bc971aa70940724fc35def711cc6084663
cgombauld <clement.gombauld@cpexterne.org>
2023-11-10 10:10:27 +0100

    get account details fully implemented

--- src/main/java/app/netlify/clementgombauld/banking/account/usecases/queries/GetAccountDetails.java
+++ src/main/java/app/netlify/clementgombauld/banking/account/usecases/queries/GetAccountDetails.java
@@ -1,8 +1,11 @@
 package app.netlify.clementgombauld.banking.account.usecases.queries;
 
 import app.netlify.clementgombauld.banking.account.domain.AuthenticationGateway;
+import app.netlify.clementgombauld.banking.account.domain.Bic;
 import app.netlify.clementgombauld.banking.account.domain.Customer;
 import app.netlify.clementgombauld.banking.account.domain.QueryExecutor;
+import app.netlify.clementgombauld.banking.account.domain.exceptions.NoCurrentCustomerException;
+import app.netlify.clementgombauld.banking.account.domain.exceptions.UnknownAccountWithCustomerId;
 import app.netlify.clementgombauld.banking.account.rest.account.out.AccountDetailsDto;
 import org.iban4j.Iban;
 
@@ -19,16 +22,20 @@ public class GetAccountDetails {
 
     public AccountDetailsDto handle(String bic) {
 
-        Customer customer = authenticationGateway.currentCustomer().orElseThrow();
+        Bic validBic = new Bic(bic);
 
-        Iban iban = queryExecutor.findIbanByCustomerId(customer.getId()).orElseThrow();
+        Customer customer = authenticationGateway.currentCustomer()
+                .orElseThrow(NoCurrentCustomerException::new);
+
+        Iban iban = queryExecutor.findIbanByCustomerId(customer.getId())
+                .orElseThrow(() -> new UnknownAccountWithCustomerId(customer.getId()));
 
         return new AccountDetailsDto(
                 customer.getFirstName(),
                 customer.getLastName(),
                 iban.toString(),
                 iban.getAccountNumber(),
-                bic
+                validBic.value()
         );
     }
 }
--- src/test/java/app/netlify/clementgombauld/banking/account/unit/usecases/queries/GetAccountDetailsTest.java
+++ src/test/java/app/netlify/clementgombauld/banking/account/unit/usecases/queries/GetAccountDetailsTest.java
@@ -3,6 +3,9 @@ package app.netlify.clementgombauld.banking.account.unit.usecases.queries;
 import app.netlify.clementgombauld.banking.account.domain.AuthenticationGateway;
 import app.netlify.clementgombauld.banking.account.domain.Customer;
 import app.netlify.clementgombauld.banking.account.domain.QueryExecutor;
+import app.netlify.clementgombauld.banking.account.domain.exceptions.InvalidBicException;
+import app.netlify.clementgombauld.banking.account.domain.exceptions.NoCurrentCustomerException;
+import app.netlify.clementgombauld.banking.account.domain.exceptions.UnknownAccountWithCustomerId;
 import app.netlify.clementgombauld.banking.account.rest.account.out.AccountDetailsDto;
 import app.netlify.clementgombauld.banking.account.unit.inmemory.InMemoryAuthenticationGateway;
 import app.netlify.clementgombauld.banking.account.unit.inmemory.InMemoryQueryExecutor;
@@ -13,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GetAccountDetailsTest {
 
@@ -27,21 +31,12 @@ public class GetAccountDetailsTest {
 
         String bic = "AGRIFRPP989";
 
-        AuthenticationGateway authenticationGateway = new InMemoryAuthenticationGateway(
-                new Customer(
-                        customerId,
-                        firstName,
-                        lastName
-                )
 
-        );
-
-        QueryExecutor queryExecutor = new InMemoryQueryExecutor(Map.of(
+        GetAccountDetails getAccountDetails = buildGetAccountDetails(new Customer(
                 customerId,
-                Iban.valueOf("FR1420041010050500013M02606")
-        ));
-
-        GetAccountDetails getAccountDetails = new GetAccountDetails(authenticationGateway, queryExecutor);
+                firstName,
+                lastName
+        ), customerId, iban);
 
         AccountDetailsDto accountDetailsDto = getAccountDetails.handle(bic);
 
@@ -55,4 +50,43 @@ public class GetAccountDetailsTest {
                 )
         );
     }
+
+    @Test
+    void shouldNotGetAnyAccountDetailsWhenThereIsNoCustomerAuthenticated() {
+        GetAccountDetails getAccountDetails = buildGetAccountDetails(null, "", "FR1420041010050500013M02606");
+        assertThatThrownBy(() -> getAccountDetails.handle("AGRIFRPP989"))
+                .isInstanceOf(NoCurrentCustomerException.class)
+                .hasMessage("No current customer authenticated.");
+    }
+
+    @Test
+    void shouldNotGetAnyAccountDetailsWhenThereIsNoAccountIbanFound() {
+        String customerId = "3";
+        GetAccountDetails getAccountDetails = buildGetAccountDetails(new Customer(customerId, "Arsene", "Lupin"), "2", "FR1420041010050500013M02606");
+        assertThatThrownBy(() -> getAccountDetails.handle("AGRIFRPP989"))
+                .isInstanceOf(UnknownAccountWithCustomerId.class)
+                .hasMessage("There is no account with the customerId: " + customerId);
+    }
+
+    @Test
+    void shouldNotGetAnyAccountDetailsWhenThereIsAnInvalidBic() {
+        String customerId = "3";
+        GetAccountDetails getAccountDetails = buildGetAccountDetails(new Customer(customerId, "Arsene", "Lupin"), "2", "FR1420041010050500013M02606");
+        assertThatThrownBy(() -> getAccountDetails.handle("AGRIFR"))
+                .isInstanceOf(InvalidBicException.class)
+                .hasMessage("bic: AGRIFR is invalid.");
+    }
+
+    private GetAccountDetails buildGetAccountDetails(Customer customer, String customerId, String iban) {
+        AuthenticationGateway authenticationGateway = new InMemoryAuthenticationGateway(
+                customer
+        );
+        QueryExecutor queryExecutor = new InMemoryQueryExecutor(Map.of(
+                customerId,
+                Iban.valueOf(iban)
+        ));
+        return new GetAccountDetails(authenticationGateway, queryExecutor);
+    }
+
+
 }
