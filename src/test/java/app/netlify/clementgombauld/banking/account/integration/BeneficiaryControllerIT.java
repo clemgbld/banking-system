34f2a092b3e08c41ecc332cb4dba7b72ce49c9b5
cgombauld <clement.gombauld@cpexterne.org>
2023-11-10 20:30:07 +0100

    refacto

--- src/test/java/app/netlify/clementgombauld/banking/account/integration/BeneficiaryControllerIT.java
+++ src/test/java/app/netlify/clementgombauld/banking/account/integration/BeneficiaryControllerIT.java
@@ -11,7 +11,6 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.context.Lifecycle;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
